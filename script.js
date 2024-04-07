ed05a42dee33c3c47a7ca6dedf83bddd5313a84b
Soudarjya <soudyg707@gmail.com>
2024-04-07 19:24:43 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,215 @@
+
+const codes = {
+
+}
+// let countryurl="https://flagsapi.com/BE/flat/64.png";
+// (async function (){
+//     let prom1= await fetch(countryurl);
+//     let km= await prom1.json();
+//     console.log(km);})();
+
+const countryList = {
+    INR: "IN",
+    AED: "AE",
+    AFN: "AF",
+    XCD: "AG",
+    ALL: "AL",
+    AMD: "AM",
+    ANG: "AN",
+    AOA: "AO",
+    AQD: "AQ",
+    ARS: "AR",
+    AUD: "AU",
+    AZN: "AZ",
+    BAM: "BA",
+    BBD: "BB",
+    BDT: "BD",
+    XOF: "BE",
+    BGN: "BG",
+    BHD: "BH",
+    BIF: "BI",
+    BMD: "BM",
+    BND: "BN",
+    BOB: "BO",
+    BRL: "BR",
+    BSD: "BS",
+    NOK: "BV",
+    BWP: "BW",
+    BYR: "BY",
+    BZD: "BZ",
+    CAD: "CA",
+    CDF: "CD",
+    XAF: "CF",
+    CHF: "CH",
+    CLP: "CL",
+    CNY: "CN",
+    COP: "CO",
+    CRC: "CR",
+    CUP: "CU",
+    CVE: "CV",
+    CYP: "CY",
+    CZK: "CZ",
+    DJF: "DJ",
+    DKK: "DK",
+    DOP: "DO",
+    DZD: "DZ",
+    ECS: "EC",
+    EEK: "EE",
+    EGP: "EG",
+    ETB: "ET",
+    EUR: "FR",
+    FJD: "FJ",
+    FKP: "FK",
+    GBP: "GB",
+    GEL: "GE",
+    GGP: "GG",
+    GHS: "GH",
+    GIP: "GI",
+    GMD: "GM",
+    GNF: "GN",
+    GTQ: "GT",
+    GYD: "GY",
+    HKD: "HK",
+    HNL: "HN",
+    HRK: "HR",
+    HTG: "HT",
+    HUF: "HU",
+    IDR: "ID",
+    ILS: "IL",
+    IQD: "IQ",
+    IRR: "IR",
+    ISK: "IS",
+    JMD: "JM",
+    JOD: "JO",
+    JPY: "JP",
+    KES: "KE",
+    KGS: "KG",
+    KHR: "KH",
+    KMF: "KM",
+    KPW: "KP",
+    KRW: "KR",
+    KWD: "KW",
+    KYD: "KY",
+    KZT: "KZ",
+    LAK: "LA",
+    LBP: "LB",
+    LKR: "LK",
+    LRD: "LR",
+    LSL: "LS",
+    LTL: "LT",
+    LVL: "LV",
+    LYD: "LY",
+    MAD: "MA",
+    MDL: "MD",
+    MGA: "MG",
+    MKD: "MK",
+    MMK: "MM",
+    MNT: "MN",
+    MOP: "MO",
+    MRO: "MR",
+    MTL: "MT",
+    MUR: "MU",
+    MVR: "MV",
+    MWK: "MW",
+    MXN: "MX",
+    MYR: "MY",
+    MZN: "MZ",
+    NAD: "NA",
+    XPF: "NC",
+    NGN: "NG",
+    NIO: "NI",
+    NPR: "NP",
+    NZD: "NZ",
+    OMR: "OM",
+    PAB: "PA",
+    PEN: "PE",
+    PGK: "PG",
+    PHP: "PH",
+    PKR: "PK",
+    PLN: "PL",
+    PYG: "PY",
+    QAR: "QA",
+    RON: "RO",
+    RSD: "RS",
+    RUB: "RU",
+    RWF: "RW",
+    SAR: "SA",
+    SBD: "SB",
+    SCR: "SC",
+    SDG: "SD",
+    SEK: "SE",
+    SGD: "SG",
+    SKK: "SK",
+    SLL: "SL",
+    SOS: "SO",
+    SRD: "SR",
+    STD: "ST",
+    SVC: "SV",
+    SYP: "SY",
+    SZL: "SZ",
+    THB: "TH",
+    TJS: "TJ",
+    TMT: "TM",
+    TND: "TN",
+    TOP: "TO",
+    TRY: "TR",
+    TTD: "TT",
+    TWD: "TW",
+    TZS: "TZ",
+    UAH: "UA",
+    UGX: "UG",
+    USD: "US",
+    UYU: "UY",
+    UZS: "UZ",
+    VEF: "VE",
+    VND: "VN",
+    VUV: "VU",
+    YER: "YE",
+    ZAR: "ZA",
+    ZMK: "ZM",
+    ZWD: "ZW",
+};
+let im1=document.querySelector('.img1');
+let im2=document.querySelector('.img2');
+let dropdown = document.querySelectorAll('select');
+let but = document.querySelector('button');
+for (j of dropdown) {
+    for (i in countryList) {
+        let select = document.createElement('option');
+        j.appendChild(select);
+        select.innerText = i;
+    }
+}
+async function flag(o,g){
+    g.src=`https://flagsapi.com/${o}/flat/64.png`;
+}
+let mess = document.querySelector('.message');
+let from = document.querySelector('#from');
+let to = document.querySelector('#to');
+let inp = document.querySelector('input');
+let url = `https://open.er-api.com/v6/latest/${from.value}`;;
+let r = to.value;
+let imgurl = "https://flagsapi.com/BE/shiny/64.png";
+from.addEventListener('change', () => {
+        url = `https://open.er-api.com/v6/latest/${from.value}`;
+        flag(from.value.slice(0,-1),im1);
+    })
+to.addEventListener('change', () => {
+    r = to.value;
+    flag(r.slice(0,-1),im2);
+})
+async function updateMessage() {
+    let prom = await fetch(url);
+    let js = await prom.json();
+    mess.innerText = (js.rates[r] * inp.value);
+}
+but.addEventListener('click', (evt) => {
+    evt.preventDefault();
+    updateMessage();
+});
+window.addEventListener('load', updateMessage);
+let currencyurl = ' https://open.er-api.com/v6/latest/INR';
+// (async function (){
+//     let prom= await fetch(currencyurl);
+//     let km= await prom.json();
+//     console.log(km.rates.AED);})();
